4b99dd28fd0213fe09f719efd3cda6b8e2986607
Fredy ID <56205156+fredy-ID@users.noreply.github.com>
2023-08-07 11:43:59 +0400

    Revert "Revert "Update views.py""
    This reverts commit b75fc70cdfe4bfaef07fd437153a06e1c2bfd3e1.

--- scanner/views.py
+++ scanner/views.py
@@ -75,7 +75,9 @@ class ScannerView(generics.CreateAPIView):
             
             prediction = model.predict(image_array)
             
-            predicted_class_index = np.argmax(prediction)
+            probs = np.exp(prediction) / np.sum(np.exp(prediction), axis=1, keepdims=True)
+            
+            predicted_class_index = np.argmax(probs, axis=1)
             predicted_class_name = self.class_names[predicted_class_index]
             
             return Response(
